c6160dfa1ec35cb8b282a8a84ba8580bcf9ca983
StevenHernandezJr <42784972+StevenHernandezJr@users.noreply.github.com>
2018-09-11 18:25:06 -0500

    Adding IEmployee

---
+++ ThreeSolidPrinciples/IEmployee.java
@@ -0,0 +1,11 @@
+/*
+This class is a employee class which is an implementation of Iworker class.
+Which has a method of eat.
+*/
+
+
+package threeSolid;
+
+public interface IEmployee {
+	public void eat();
+}
