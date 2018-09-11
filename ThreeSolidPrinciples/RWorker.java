4aac139d0dcb4c1f79de632bcebf745d00c0c192
StevenHernandezJr <42784972+StevenHernandezJr@users.noreply.github.com>
2018-09-11 18:53:07 -0500

    Added to Package

--- ThreeSolidPrinciples/RWorker.java
+++ ThreeSolidPrinciples/RWorker.java
@@ -1,6 +1,9 @@
 /*
  * This class is a seperate interface to keep robot workers away from human worker and their properties
  */
+
+package threesolid;
+
 public interface RWorker {
     public void work();
 }
