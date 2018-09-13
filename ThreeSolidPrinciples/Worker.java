e007de5215212a413c2c765a5873d68d9e2ae408
StevenHernandezJr <42784972+StevenHernandezJr@users.noreply.github.com>
2018-09-13 11:30:21 -0500

    Adding Worker class

---
+++ ThreeSolidPrinciples/Worker.java
@@ -0,0 +1,11 @@
+package threesolid;
+
+public class Worker extends BaseWorker implements IEat,ISick {
+	public void eat() {
+		//Worker eats
+	}
+	
+	public void sick() {
+		//Worker is sick
+	}
+}
