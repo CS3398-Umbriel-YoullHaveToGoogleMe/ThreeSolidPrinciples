f8acbec4eb0e327e0692734eedd36113c6b82a7b
StevenHernandezJr <42784972+StevenHernandezJr@users.noreply.github.com>
2018-09-13 13:29:50 -0500

    Adding Comment
    Added a comment block at the top of the file with a description of the TempWorker class.

--- ThreeSolidPrinciples/TempWorker.java
+++ ThreeSolidPrinciples/TempWorker.java
@@ -1,3 +1,11 @@
+/************************************************************************
+Steven Hernandez
+This file contains the TempWorker class that inherits from the BaseWorker
+class.  However, this class does override the work method with its own
+implementation of that function.  This class also implements the eat()
+and sick() methods from the IEat and ISick interfaces.
+************************************************************************/
+
 package threesolid;
 
 public class TempWorker extends BaseWorker implements IEat,ISick {
