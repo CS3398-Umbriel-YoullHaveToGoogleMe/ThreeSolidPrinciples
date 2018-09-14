b419a121a074e69162dee9af2e18db497c644cf9
mma71 <41317281+mma71@users.noreply.github.com>
2018-09-14 18:24:19 -0500

    part e of hmwk 5
    changes to main.java

--- ThreeSolidPrinciples/Main.java
+++ ThreeSolidPrinciples/Main.java
@@ -5,31 +5,62 @@ may not be able to apply the single responsilbity principle here. Interface Segr
 
 package threesolid;
 
-public class Main {   
-   // The entry main() method
-   public static void main(String[] args) {
-	   Manager tsManager = new Manager();
- 
-	   try 
-	   {
-		   System.out.format("Starting ... \n");               
-	   } 
-	   catch (Exception main_except)
-	   {
-		   main_except.printStackTrace();
-	   }
-      
-	   try 
-	   {
-		   System.out.format("Stopping ... \n");               
-	   } 
-	   catch (Exception main_except)
-	   {
-		   main_except.printStackTrace();
-	   }
+public class Main {
+    // The entry main() method
+    public static void main(String[] args) {
+        //Manager tsManager = new Manager();
 
-	   System.exit(0);
-   }
+        try
+        {
+            System.out.format("Starting ... \n");
+        }
+        catch (Exception main_except)
+        {
+            main_except.printStackTrace();
+        }
+
+        Manager tsManager1 = new Manager();
+        Worker worker1 =  new Worker();
+        tsManager1.setWorker(worker1);
+        tsManager1.manage();
+        worker1.eat();
+        worker1.sick();
+
+        ProjectManager projMang = new ProjectManager();
+        SuperWorker supWorker1 = new SuperWorker();
+        projMang.setWorker(supWorker1);
+        projMang.manage();
+        projMang.schedulework();
+        supWorker1.eat();
+        supWorker1.sick();
+        supWorker1.work();
+
+        ProductManager prodMang = new ProductManager();
+        Robot rob = new Robot();
+        prodMang.setWorker(rob);
+        prodMang.manage();
+        prodMang.defineproduct();
+        rob.reboot();
+
+        Manager tsManager2 = new Manager();
+        TempWorker tempWorker1 = new TempWorker();
+        tsManager2.setWorker(tempWorker1);
+        tsManager2.manage();
+        tempWorker1.eat();
+        tempWorker1.sick();
+        tempWorker1.work();
+
+
+        try
+        {
+            System.out.format("Stopping ... \n");
+        }
+        catch (Exception main_except)
+        {
+            main_except.printStackTrace();
+        }
+
+        System.exit(0);
+    }
 };
 
- 
