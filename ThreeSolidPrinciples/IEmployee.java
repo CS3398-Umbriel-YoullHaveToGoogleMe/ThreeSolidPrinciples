cba96c449eafbe03170f0850fd142ce97736eb55
StevenHernandezJr <42784972+StevenHernandezJr@users.noreply.github.com>
2018-09-15 15:06:21 -0500

    Copied IWorker and IEmployee to the ThreeSolidPrinciples directory

---
+++ ThreeSolidPrinciples/IEmployee.java
@@ -0,0 +1,10 @@
+/*
+This class is a employee class which is an implementation of Iworker class.
+Which has a method of eat.
+*/
+
+package threesolid;
+
+public interface IEmployee {
+	public void eat();
+}
