676664de3b598b62c37c1f2d90a8c76a5bd938a4
nmanasiya <42896536+nmanasiya@users.noreply.github.com>
2018-10-24 13:30:41 -0500

    temperary changes to deal with dul class error

--- src/RWorker.java
+++
@@ -1,9 +0,0 @@
-/*
- * This class is a seperate interface to keep robot workers away from human worker and their properties
- */
-
-package threesolid;
-
-public interface RWorker {
-    public void work();
-}
