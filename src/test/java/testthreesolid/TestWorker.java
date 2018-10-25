fadcd55b79a7b0e00cacb612a242e8b616ec9956
Aadil Khatri <38430842+AadilKhatri@users.noreply.github.com>
2018-10-25 18:23:07 -0500

    new test

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -17,9 +17,18 @@ public class TestWorker {
 
   public void testWorkMethod()
   {
-    assertEquals(0, multiply(10, 0), "10 x 0 must be 0");
+  //  assertEquals(0, multiply(10, 0), "10 x 0 must be 0");
     assertEquals("Worker working...","I'm working already!","Mismatch between test text and method text");
 
   }
+  @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        MyClass tester = new MyClass(); // MyClass is tested
+
+        // assert statements
+        assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
+        assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
+        assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+    }
 
 }
