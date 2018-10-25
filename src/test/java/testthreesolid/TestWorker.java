0b7e13da5e07539c63940bc0fb8745dd2e35fc02
Aadil Khatri <38430842+AadilKhatri@users.noreply.github.com>
2018-10-25 17:52:00 -0500

    just testing

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -6,15 +6,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestWorker {
-    
+  
+
   private Worker w = new Worker();
-    
+
   @Test
   @DisplayName("Work Method Test")
   public void testWorkMethod()
   {
     assertEquals("Worker working...","I'm working already!","Mismatch between test text and method text");
-                 
+
   }
 
-}  
+}
