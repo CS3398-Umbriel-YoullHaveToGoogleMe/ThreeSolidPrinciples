57d2a9cab94f317c3835ba9133731d3465d0a336
Aadil Khatri <38430842+AadilKhatri@users.noreply.github.com>
2018-10-25 18:27:19 -0500

    changed it back to what i had

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -21,14 +21,4 @@ public class TestWorker {
     assertEquals("Worker working...","I'm working already!","Mismatch between test text and method text");
 
   }
-  @Test
-    public void multiplicationOfZeroIntegersShouldReturnZero() {
-        MyClass tester = new MyClass(); // MyClass is tested
-
-        // assert statements
-        assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
-        assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
-        assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
-    }
-
 }
