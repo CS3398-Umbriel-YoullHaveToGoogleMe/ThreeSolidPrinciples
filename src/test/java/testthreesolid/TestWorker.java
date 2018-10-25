1fc1964a716aa8742519547ca72ede2116ebd7a1
Aadil Khatri <38430842+AadilKhatri@users.noreply.github.com>
2018-10-25 18:15:38 -0500

    Update src/test/java/testthreesolid/TestWorker.java

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -13,10 +13,11 @@ public class TestWorker {
   @Test
   @DisplayName("Work Method Test")
 //  public void multiplicationOfZeroIntegersShouldReturnZero() {
-    
+
 
   public void testWorkMethod()
   {
+    assertEquals(0, Worker.multiply(10, 0), "10 x 0 must be 0");
     assertEquals("Worker working...","I'm working already!","Mismatch between test text and method text");
 
   }
