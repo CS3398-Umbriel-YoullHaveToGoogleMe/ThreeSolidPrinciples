83bafe14cc69ff354afea6ecc5dae3b0993f873a
M-Stephens61 <42839454+M-Stephens61@users.noreply.github.com>
2018-10-25 18:28:59 -0500

    run dammit

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.Test;
 public class TestWorker {
   
 
-  private Worker w = new Worker();
+  private BaseWorker w = new BaseWorker();
 
   @Test
   @DisplayName("Work Method Test")
   public void testWorkMethod()
   {
-    assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
+    assertEquals(w.work(),"Working...","Mismatch between test text and method text");
 
   }
 
