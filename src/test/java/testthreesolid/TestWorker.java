68c316640caf172e2aeca466194fc642bdeb9b4a
Aadil Khatri <38430842+AadilKhatri@users.noreply.github.com>
2018-10-25 16:42:10 -0500

    Trying my test using robot class

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestWorker {
-    
-  private Worker w = new Worker();
-    
+
+  private Robot w = new Robot();
+
   @Test
-  @DisplayName("Work Method Test")
-  public void testWorkMethod()
+  @DisplayName("Robot Method Test")
+  public void testRobotMethod()
   {
-    assertEquals("Worker working...","I'm working already!","Mismatch between test text and method text");
-                 
+  //  assertEquals("Worker working...","I'm working already!","Mismatch between test text and method text");
+    assertEquals(w.reboot(),"I am already rebooting!","Mismatch between test text and method text");
   }
 
-}  
+}
