7e913c755a73763c1c1370227b7d3d49943a3a87
Aadil Khatri <38430842+AadilKhatri@users.noreply.github.com>
2018-10-25 18:08:08 -0500

    Trying a test

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -7,14 +7,24 @@ import org.junit.jupiter.api.Test;
 
 public class TestWorker {
 
-  private Robot w = new Robot();
+
+  private Worker w = new Worker();
 
   @Test
-  @DisplayName("Robot Method Test")
-  public void testRobotMethod()
+  @DisplayName("Work Method Test")
+  public void multiplicationOfZeroIntegersShouldReturnZero() {
+       MyClass tester = new MyClass(); // MyClass is tested
+
+       // assert statements
+       assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
+       assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
+       assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+   }
+
+  public void testWorkMethod()
   {
-  //  assertEquals("Worker working...","I'm working already!","Mismatch between test text and method text");
-    assertEquals("Robot rebooting....","I am already rebooting!","Mismatch between test text and method text");
+    assertEquals("Worker working...","I'm working already!","Mismatch between test text and method text");
+
   }
 
 }
