ef5b9a9b29a89e4b141a727bfebd55b4d04403af
Aadil Khatri <38430842+AadilKhatri@users.noreply.github.com>
2018-10-26 17:47:06 -0500

    Update src/main/java/IWorker.java

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -8,15 +8,34 @@ import org.junit.jupiter.api.Test;
 public class TestWorker {
 
 
-  private BaseWorker w = new BaseWorker();
+    private BaseWorker w = new BaseWorker();
 
-  @Test
-  @DisplayName("Work Method Test")
-  public void testWorkMethod()
-  {
-    assertEquals(w.work(),"Working...","Mismatch between test text and method text");
+    @Test
+    @DisplayName("Work Method Test")
+    public void testWorkMethod()
+    {
+        assertEquals(w.work(),"Working...","Mismatch between test text and method text");
 
-  }
+    }
+
+<<<<<<< HEAD
+    private SuperWorker sw = new SuperWorker();
+
+    @Test
+    @DisplayName("(M. Stephens)SuperWorker_Test")
+    public void testSuperWorkerEatMethod()
+    {
+       assertEquals(sw.eat(),"SuperWorker is soopa eating...","Mismatch between test text and method text");
+
+    }
+
+    @Test
+    @DisplayName("(M. Stephens)SuperWorker_Test")
+    public void testWillFailSuperWorkerEatMethod()
+    {
+        assertEquals(sw.work(),"Destined to Fail...","Mismatch between test text and method text");
+
+    }
 }
 
 //  private SuperWorker sw = new SuperWorker();
