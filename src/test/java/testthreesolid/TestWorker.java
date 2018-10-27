64fb6fe35a370e954fbceaacb279ce9390631dfb
StevenHernandezJr <42784972+StevenHernandezJr@users.noreply.github.com>
2018-10-26 23:18:41 -0500

    Changes to TestWorker.java

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -12,17 +12,24 @@ public class TestWorker {
 
   @Test
   @DisplayName("Work Method Test")
-  public void testWorkMethod()
-  {
-    assertEquals("Worker working...","I'm working already!","Mismatch between test text and method text");
-
+  public void testWorkMethod() {
+	  assertEquals("Worker working...","I'm working already!","Mismatch between test text and method text");
   }
   
   @Test
   @DisplayName("Test that BaseWorker Object Created")
   public void newtestSHWorkerPass() {
 	  BaseWorker worker = new BaseWorker();
+	  
 	  assertFalse((worker == null), "worker variable should contain a BaseWorker object");
   }
-
+  
+  @Test
+  @DisplayName("Test that two BaseWorker objects are different and not the same object.")
+  public void newtestSHWorkerFail() {
+	  BaseWorker worker1 = new BaseWorker();
+	  BaseWorker worker2 = new BaseWorker();
+	  
+	  assertEquals(worker1, worker2, "worker1 and worker2 are not the same object.");
+  }
 }
